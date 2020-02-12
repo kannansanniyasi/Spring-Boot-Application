2d1771bd0d78cba5b3b2fe6a288f8f8dace2dbaf
kannansanniyasi <60571705+kannansanniyasi@users.noreply.github.com>
2020-02-13 01:09:43 +0800

    Create WeatherVo.java

---
+++ WeatherVo.java
@@ -0,0 +1,36 @@
+package com.app.vo;
+
+public class WeatherVo {
+	
+	private int sNo;
+	private String country;
+	private String city;
+	private String weather;
+	public int getsNo() {
+		return sNo;
+	}
+	public void setsNo(int sNo) {
+		this.sNo = sNo;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getWeather() {
+		return weather;
+	}
+	public void setWeather(String weather) {
+		this.weather = weather;
+	}
+	
+	
+
+}
